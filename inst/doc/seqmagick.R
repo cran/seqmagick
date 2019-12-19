76957950feeb8e74149f5e249648fa060b2d4009
Guangchuang Yu <guangchuangyu@gmail.com>
2019-12-19 12:40:02 +0000

    version 0.1.3

--- inst/doc/seqmagick.R
+++ inst/doc/seqmagick.R
@@ -5,7 +5,6 @@ knitr::opts_chunk$set(tidy = FALSE,
 ## ----echo=FALSE, results="hide", message=FALSE--------------------------------
 library(magrittr)
 library(Biostrings)
-library(muscle)
 library("seqmagick")
 
 ## -----------------------------------------------------------------------------
@@ -16,47 +15,38 @@ download_genbank(acc='AB115403', format='fasta', outfile=tmpfa)
 readLines(tmpgb)[1:10]
 readLines(tmpfa)
 
-## -----------------------------------------------------------------------------
-fa_file <- system.file("extdata/HA.fas", package="seqmagick")
-## use the small subset to save compilation time of the vignette
-fa2 <- tempfile(fileext = '.fa')
-fa_read(fa_file) %>% bs_filter('ATGAAAGTAAAA', by='sequence') %>% fa_write(fa2, type='interleaved')
+## ----eval=F-------------------------------------------------------------------
+#  fa_file <- system.file("extdata/HA.fas", package="seqmagick")
+#  ## use the small subset to save compilation time of the vignette
+#  fa2 <- tempfile(fileext = '.fa')
+#  fa_read(fa_file) %>% bs_filter('ATGAAAGTAAAA', by='sequence') %>% fa_write(fa2, type='interleaved')
+#  
+#  
+#  alnfas <- tempfile(fileext = ".fas")
+#  fa_read(fa2) %>% bs_aln(quiet=TRUE) %>% fa_write(alnfas)
+#  
+#  ## phylip format is only for aligned sequences
+#  tmpphy <- tempfile(fileext = ".phy")
+#  fas2phy(alnfas, tmpphy, type = 'sequential')
 
+## ----eval=F-------------------------------------------------------------------
+#  phy2fas(tmpphy, alnfas, type = 'interleaved')
 
-alnfas <- tempfile(fileext = ".fas")
-fa_read(fa2) %>% bs_aln(quiet=TRUE) %>% fa_write(alnfas)
+## ----eval=F-------------------------------------------------------------------
+#  tmpfas <- tempfile(fileext='.fa')
+#  fa_read(fa2) %>% fa_write(tmpfas, type="sequential")
 
-## phylip format is only for aligned sequences
-tmpphy <- tempfile(fileext = ".phy")
-fas2phy(alnfas, tmpphy, type = 'sequential')
-readLines(tmpphy)[1:3]
+## ----eval=F-------------------------------------------------------------------
+#  tmpphy2 <- tempfile(fileext = '.phy')
+#  phy_read(tmpphy) %>% phy_write(tmpphy2, type="interleaved")
 
-## -----------------------------------------------------------------------------
-phy2fas(tmpphy, alnfas, type = 'interleaved')
-readLines(alnfas)[1:4]
-
-## -----------------------------------------------------------------------------
-readLines(fa2)[1:10]
-
-## -----------------------------------------------------------------------------
-tmpfas <- tempfile(fileext='.fa')
-fa_read(fa2) %>% fa_write(tmpfas, type="sequential")
-readLines(tmpfas)[1:4]
-
-## -----------------------------------------------------------------------------
-tmpphy2 <- tempfile(fileext = '.phy')
-phy_read(tmpphy) %>% phy_write(tmpphy2, type="interleaved")
-readLines(tmpphy2)[1:4]
-
-## -----------------------------------------------------------------------------
-bs <- fa_read(fa_file)
-bs_filter(bs, 'ATGAAAGTAAAA', by='sequence')
-
-aln <- bs_filter(bs, 'ATGAAAGTAAAA', by='sequence') %>% bs_aln(quiet=TRUE)
-
-aln
-
-bs_consensus(aln)
+## ----eval=F-------------------------------------------------------------------
+#  bs <- fa_read(fa_file)
+#  bs_filter(bs, 'ATGAAAGTAAAA', by='sequence')
+#  
+#  aln <- bs_filter(bs, 'ATGAAAGTAAAA', by='sequence') %>% bs_aln(quiet=TRUE)
+#  
+#  bs_consensus(aln)
 
 ## ----echo=FALSE---------------------------------------------------------------
 sessionInfo()
